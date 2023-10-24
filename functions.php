a33b044598a9995ccbf8c568f4e2c2fec6893e17
Samara-Alves <samara_alves28@hotmail.com>
2023-10-24 15:45:12 -0300

    ajuste das paginas

--- functions.php
+++
@@ -1,9 +0,0 @@
-
-
-
-function video_embed_responsivo( $html ) 
-{
-return '<div class="container-video">' . $html . '</div>';
-}
-add_filter( 'embed_embed_html', 'video_embed_responsivo', 10, 3 );
-add_filter( 'video_embed_html', 'video_embed_responsivo' );
